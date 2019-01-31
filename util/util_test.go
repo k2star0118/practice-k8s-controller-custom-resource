7a88c16fb9137149e206cd15b44e9bdcce3756b9
ken.liu <ken.liu@geminidata.com>
2019-01-31 23:37:41 +0800

    Add one test case

---
+++ util/util_test.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestGetKubernetesConfig(t *testing.T) {
+	_, err := os.Stat(os.Getenv("HOME") + "/.kube/config")
+	_, configErr := GetKubernetesConfig()
+	if err == nil {
+		assert.Nil(t, configErr)
+	} else if os.IsNotExist(err) {
+		assert.NotNil(t, configErr)
+	}
+}
+
+func TestGetKubernetesClient(t *testing.T) {
+	_, err := os.Stat(os.Getenv("HOME") + "/.kube/config")
+	_, configErr := GetKubernetesClient()
+
+	if err == nil {
+		assert.Nil(t, configErr)
+	} else if os.IsNotExist(err) {
+		assert.NotNil(t, configErr)
+	}
+}
+
+/*func TestGetMyKubernetesClient(t *testing.T) {
+	_, err := os.Stat(os.Getenv("HOME") + "/.kube/config")
+	_, configErr := util.GetMyKubernetesClient()
+
+	if err == nil {
+		assert.Nil(t, configErr)
+	} else if os.IsNotExist(err) {
+		assert.NotNil(t, configErr)
+	}
+}*/
